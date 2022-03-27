c77bdba179753b1ad2a78a048c90633a9c812172
Janith Harinda <jharinda@gmail.com>
2022-03-27 16:23:06 +0530

    oon

--- server.js
+++ server.js
@@ -86,5 +86,5 @@ app.use("/peerServer", peerServer);
 const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
-     console.log("server running on ", PORT);
+     console.log("server running oon ", PORT);
 });
